8a08e317b1616571ef437fc50f46a9d23b2f2597
da-tsut <1potatohead2hamm@gmail.com>
2020-11-08 17:58:52 +0900

    新規登録又ログイン時のエラー発火

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name
+  validates :family_name
+  validates :first_name_kana
+  validates :family_name_kana
+  end
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' パスワードは半角英数字混合であること' } do
+    validates :password
+  end
+
+
 end
