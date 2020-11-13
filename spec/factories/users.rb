d5bf561334dbcda1aba1cbca6c08b5b8b2c03488
da-tsut <1potatohead2hamm@gmail.com>
2020-11-13 21:54:57 +0900

    テストコードレビュー１

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    nickname {Faker::Internet.username}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password  {"aA1#{ Faker::Internet.password}"}
+    #password {password}
     password_confirmation {password}
+    first_name {'健'}
+    family_name {'佐藤'}
+    first_name_kana {'タケル'}
+    family_name_kana {'サトウ'}
+    birth_year_month_day {'1982-03-20'}
   end
 end
