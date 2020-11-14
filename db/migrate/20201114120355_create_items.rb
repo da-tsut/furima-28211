2e296ef42969fed35274f7119c7d9342aff6aaaf
da-tsut <1potatohead2hamm@gmail.com>
2020-11-14 22:23:23 +0900

    itemモデルの作成（SequelPro）

--- db/migrate/20201114120355_create_items.rb
+++ db/migrate/20201114120355_create_items.rb
@@ -1,9 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :content
-      t.references :user, foreign_key: true
-      t.timestamps
+      t.string  :name,                  null: false
+      t.text    :introduction ,         null: false
+      t.integer :price,                 null: false
+      t.integer :item_condition_id,     null: false
+      t.integer :postage_payer_id,      null: false
+      t.integer :prefecture_code_id,    null: false
+      t.integer :item_purchase_id,      null: false
+      t.integer :price_preservation_id, null: false
+      t.integer :user_id,               null: false, foreign_key: true
     end
   end
 end
