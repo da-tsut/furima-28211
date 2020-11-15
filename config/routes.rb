8262eae6bb2c43ac7f005635585c585c3e9280de
da-tsut <1potatohead2hamm@gmail.com>
2020-11-15 11:01:18 +0900

    現在不使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  resources :users, only: [:edit, :update]
-  resources :items, only: [:new, :create] 
+#  devise_for :users
+#  root to: "items#index"
+#  resources :users, only: [:edit, :update]
+#  resources :items, only: [:new, :create] 
   # do
   #resources :messages, only: [:index, :create]
-  end
+#  end
 end
