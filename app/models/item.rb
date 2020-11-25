5421f7b1f32aadf370a9043a2c45b76441e8ffaa
da-tsut <1potatohead2hamm@gmail.com>
2020-11-25 21:45:06 +0900

    不要な＃文の削除など

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,24 +12,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :day_to_ship
 
-  validates :image, presence: { message: "を添付してください"}
-  validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を記入してください"}
-  validates :introduction, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を記入してください"}
-  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :item_condition_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :postage_payer_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :day_to_ship_id, numericality: { other_than: 1, message: "を選択してください" }
-  validates :price, presence: { message: "を入力してください"}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は¥300〜9,999,999の間で入力してください"}
-
-#def save
-  # ユーザーの情報を保存し、「user」という変数に入れている
-#  user = User.create(name: name, name_reading: name_reading, nickname: nickname)
-  # 住所の情報を保存
-#  Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
-  # 寄付金の情報を保存
-#  Donation.create(price: price, user_id: user.id)
-  #  end
+  #with_options presence: true do
+    validates :image, presence: { message: "を添付してください"}
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を記入してください"}
+    validates :introduction, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を記入してください"}
   #end
+    validates :category_id,:item_condition_id,:postage_payer_id,:prefecture_id,:day_to_ship_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :price, presence: { message: "を入力してください"}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は¥300〜9,999,999の間で入力してください"}
 end
