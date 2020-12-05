b4a6c3378f08b6ebbcfd31ec65b1010febd8c00b
da-tsut <1potatohead2hamm@gmail.com>
2020-12-05 21:50:09 +0900

    商品情報編集機能のコードレビュー2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit , :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC") 
@@ -15,6 +15,11 @@ class ItemsController < ApplicationController
     end
   end
   
+
+  def show
+  end
+
+
   def create
     @item = Item.new(item_params)
     if @item.valid? 
@@ -25,13 +30,13 @@ class ItemsController < ApplicationController
     end
   end
 
-
- # def destroy
- #   item = Item.find(params[:id])
- #  item.destroy
- # end
-
-
+  
+  # def destroy
+  #   item = Item.find(params[:id])
+  #  item.destroy
+  # end
+  
+  
   def edit
   end
 
@@ -43,22 +48,6 @@ class ItemsController < ApplicationController
         end
   end
 
-#  def create
-#    @item = Item.new(item_params)
-#    if @item.valid? 
-#       @item.save
-#       redirect_to root_path
-#    else
-#      render :new
-#    end
-#  end
-
-
-
-#  def show
-#   @item = Item.find(params[:id])
-#  end
-
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
@@ -68,9 +57,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-      def move_to_index
-        unless user_signed_in?
-          redirect_to action: :index
+  def move_to_index
+    if current_user.id != @item.user_id 
+      redirect_to action: :index
     end
   end
 end
