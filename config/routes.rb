c218501693683cf0d0c5ba8fe6e2bafddea02249
da-tsut <1potatohead2hamm@gmail.com>
2020-12-07 10:18:57 +0900

    クレジットカード機能の実装2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   root to: "items#index"
   resources :items
-  resources :orders, only:[:create]
-end
+  resources :items do
+    resources :orders, only: :create
+    collection do
+      get 'search'
+    end
+  end
+#  resources :users, only: :show
+  end
+
+
