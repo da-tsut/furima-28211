aa97047900d027a4dececa239d338b9b4a31bb29
da-tsut <1potatohead2hamm@gmail.com>
2020-12-14 14:59:46 +0900

    商品購入機能のコードレビュー2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,7 +23,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:user_order).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number,:item_purchase_id).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+    params.require(:user_order).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
   end
 
   def set_order
